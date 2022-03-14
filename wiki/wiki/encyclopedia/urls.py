4646f35c513a54b7c2f85887808e5844623ab16c
Chin Zheng Hao <chinhao61@gmail.com>
2022-03-14 21:38:29 +0800

    added add page function

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.reqSearch, name="reqSearch"),
     path("random", views.randomPage, name="randomPage"),
+    path("createPage", views.createPage, name="createPage"),
     path("<str:entryName>", views.reqEntry, name="reqEntry"),
 ]
--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -7,8 +7,15 @@ from random import randrange
 
 from . import util
 
+edited = None
+new = None
+
 class searchForm(forms.Form):
     search = forms.CharField(label="Search")
+
+class newPageForm(forms.Form):
+    title = forms.CharField(label="Title")
+    markdown = forms.CharField(widget=forms.Textarea, label="Markdown")
 
 def reqSearch(request):
     if request.method == "POST":
@@ -35,7 +42,22 @@ def randomPage(request):
     return HttpResponseRedirect(f"../{entries[index]}")
                 
 def index(request):
+    global new
+    global edited
+
+    alert = False
+    message = "foo"
+    if edited:
+        alert = True
+        message = f"Entry {edited} has been changed!"
+        edited = None
+    if new:
+        alert = True
+        message = f"A new entry has been created: {new}"
+        new = None
     return render(request, "encyclopedia/index.html", {
+        "alert": alert,
+        "message": message,
         "entries": util.list_entries(),
         "form": searchForm()
     })
@@ -50,4 +72,23 @@ def reqEntry(request, entryName):
         "entryName": entryName,
         "HTML": data,
         "form": searchForm()
+    })
+
+def createPage(request):
+    global new
+
+    if request.method == "POST":
+        form = newPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            markdown = form.cleaned_data["markdown"]
+            if not util.get_entry(title):
+                util.save_entry(title, markdown)
+                new = title
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                #todo
+                return HttpResponse("nah")
+    return render(request, "encyclopedia/createPage.html", {
+        "form": newPageForm(),
     })
